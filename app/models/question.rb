ecfdf4c5eda6940e3110ab699bbd37910d7665d9
tim kellog & abdul rahimi <student@epicodus.com>
2015-10-19 15:27:57 -0700

    add answer model

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,11 @@ class Question < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :title, :content
+
+
+  # searching
+  def self.search(query)
+    where("title like ?", "%#{query}%")
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019172800) do
+ActiveRecord::Schema.define(version: 20151019215623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "question_id"
+    t.string   "content"
+    t.integer  "votes"
+    t.datetime "created_at"
+    t.datetime "edited_at"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
