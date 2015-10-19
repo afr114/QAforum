c09eee2b72488997bca77e574ba5992ac1a104c4
tim kellog & abdul rahimi <student@epicodus.com>
2015-10-19 12:06:13 -0700

    add sessions authentication to sessions controller and add log in form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,21 +25,28 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user.update(user_params)
+      flash[:notice] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @user.destroy
+    flash[:notice] = "We are sorry to see you go."
+    redirect_to root_path
   end
 
   private
 
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
     def set_user
       @user = User.find(params[:id])
     end
-
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  root to: "questions#index"
+  root to: 'questions#index'
 
   resources :questions
   resources :users
+
+  resources :sessions
+
+  get     'login'  => 'sessions#new'
+  post    'login'  => 'sessions#create'
+  delete  'logout' => 'sessions#destroy'
+
 end
