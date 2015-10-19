6399527d0a8977d67542f5073104f5450dac1bea
tim kellog & abdul rahimi <student@epicodus.com>
2015-10-19 10:00:47 -0700

    add user model with testing specs

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019161738) do
+ActiveRecord::Schema.define(version: 20151019164947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "user_id"
+    t.string   "picture"
+    t.datetime "created_at"
+    t.datetime "edited_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -23,7 +32,7 @@ ActiveRecord::Schema.define(version: 20151019161738) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin"
+    t.boolean  "admin",           default: false
   end
 
 end
